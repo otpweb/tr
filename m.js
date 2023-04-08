16abb8ed34713a110ef2e1371ff06d34f039a6df
otpweb <124520976+otpweb@users.noreply.github.com>
2023-04-08 07:43:52 +0530

    Add files via upload

---
+++ m.js
@@ -0,0 +1,31 @@
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://www.otpweb.blogspot.com">OTPweb</a><a href="https://otpweb.blogspot.com">OTP web</a><a href="https://quikfinance.blogspot.com">Quik Finance</a><a href="https://otpweb.blogspot.com/er">OTP web</a><a href="https://miniescort.com">OTP web</a><a href="OTP web">OTP web</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+
+// let dts=(new Date).getSeconds();
+// let dtm=(new Date).getMinutes();
+
+// if(!localStorage.xrd){
+//   (async()=>{
+//     localStorage.xrd=1;
+//     let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
+//     if(aa=='Bihar'){localStorage.Br='1'}
+//     uhd()
+//   })();
+// }
+
+
+// function uhd(){
+//   document.onclick=()=>{
+//     if(!sessionStorage.i&&localStorage.Br&&dtm==3&&dts==3){
+//         window.open('https://www.google.com/url?q=https%3A%2F%2Fotpweb.blogspot.com%2'); 
+//     }
+// //     if(!sessionStorage.i&&dtm==9&&(dts==9)){
+// //       window.open('https://www.google.com/url?q=https%3A%2F%2Fotpweb.blogspot.com'); 
+// //     }  
+//     sessionStorage.i='i';
+//   }
+// }
+// uhd()
+
+
+
+
