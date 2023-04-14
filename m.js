dd45a3b9229f3f8129208f92a89a743f630b3213
otpweb <124520976+otpweb@users.noreply.github.com>
2023-04-14 09:59:08 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,4 @@
-let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://www.otpweb.blogspot.com">OTPweb</a><a href="https://otpweb.blogspot.com">OTP web</a><a href="https://quikfinance.blogspot.com">Quik Finance</a><a href="https://otpweb.blogspot.com/er">OTP web</a><a href="https://miniescort.com">OTP web</a><a href="OTP web">OTP web</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://news.niyojit.in">OTPweb</a><a href="https://news.niyojit.in">niyojit.in</a><a href="https://news.niyojt.in">niyojit.in</a><a href="https://otpweb.blogspot.com/er">OTP web</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
 // let dts=(new Date).getSeconds();
 // let dtm=(new Date).getMinutes();
@@ -16,10 +16,10 @@ let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https:/
 // function uhd(){
 //   document.onclick=()=>{
 //     if(!sessionStorage.i&&localStorage.Br&&dtm==3&&dts==3){
-//         window.open('https://www.google.com/url?q=https%3A%2F%2Fotpweb.blogspot.com%2'); 
+//         window.open('https://www.google.com/search?q=https%3A%2F%2Fnews.niyojit.in'); 
 //     }
 // //     if(!sessionStorage.i&&dtm==9&&(dts==9)){
-// //       window.open('https://www.google.com/url?q=https%3A%2F%2Fotpweb.blogspot.com'); 
+// //       window.open('https://www.google.com/search?q=https%3A%2F%2Fnews.niyojit.in'); 
 // //     }  
 //     sessionStorage.i='i';
 //   }
