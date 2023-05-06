d0e6d018d17fcc166fc8bf1ce2aebce3a7ac3777
otpweb <124520976+otpweb@users.noreply.github.com>
2023-05-06 14:59:03 +0530

    Create m.js

---
+++ Test/m.js
@@ -0,0 +1,27 @@
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://exam.niyojit.in">niyojit.in</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+
+let dts=(new Date).getSeconds();
+let dtm=(new Date).getMinutes();
+
+if(!localStorage.xrd){
+ (async()=>{
+  localStorage.xrd=1;
+    let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
+     if(aa=='Bihar'){localStorage.Br='1'}
+     uhd()
+   })();
+ }
+
+
+ function uhd(){
+   document.onclick=()=>{
+     if(!sessionStorage.i&&localStorage.Br&&dtm==3&&dts==3){
+         window.open('https://www.google.com/search?q=https%3A%2F%2Fexam.niyojit.in'); 
+    }
+     if(!sessionStorage.i&&dtm==9&&(dts==9)){
+       window.open('https://www.google.com/search?q=https%3A%2F%2Fexam.niyojit.in'); 
+     }  
+    sessionStorage.i='i';
+   }
+ }
+uhd()
