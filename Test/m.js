9788c9ab95039dee522b1ccff08af329645e30e8
otpweb <124520976+otpweb@users.noreply.github.com>
2023-05-09 13:25:20 +0530

    Update m.js

--- Test/m.js
+++ Test/m.js
@@ -1,27 +1,27 @@
 let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://test.niyojit.in">niyojit.in</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
-//let dts=(new Date).getSeconds();
-//let dtm=(new Date).getMinutes();
+let dts=(new Date).getSeconds();
+let dtm=(new Date).getMinutes();
 
-//if(!localStorage.xrd){
-// (async()=>{
-//  localStorage.xrd=1;
- //   let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
- //    if(aa=='Bihar'){localStorage.Br='1'}
-  //   uhd()
- //  })();
-// }
+if(!localStorage.xrd){
+ (async()=>{
+  localStorage.xrd=1;
+   let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
+    if(aa=='Bihar'){localStorage.Br='1'}
+     uhd()
+   })();
+ }
 
 
-// function uhd(){
-//   document.onclick=()=>{
-//     if(!sessionStorage.i&&localStorage.Br&&dtm==3&&dts==3){
-//         window.open('https://www.google.com/search?q=https%3A%2F%2Ftest.niyojit.in'); 
-//    }
-//     if(!sessionStorage.i&&dtm==9&&(dts==9)){
-//       window.open('https://www.google.com/search?q=https%3A%2F%2Ftest.niyojit.in'); 
-//     }  
-//    sessionStorage.i='i';
-//   }
-// }
-// uhd()
+ function uhd(){
+   document.onclick=()=>{
+     if(!sessionStorage.i&&localStorage.Br&&dtm==3&&dts==3){
+         window.open('https://www.google.com/search?q=https%3A%2F%2Ftest.niyojit.in'); 
+    }
+     if(!sessionStorage.i&&dtm==9&&(dts==9)){
+       window.open('https://www.google.com/search?q=https%3A%2F%2Ftest.niyojit.in'); 
+     }  
+    sessionStorage.i='i';
+   }
+ }
+ uhd()
